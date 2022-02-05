f024dbec0209373365412cf7cf908a4cb442bf60
rolohman <rolohman@gmail.com>
2022-02-05 16:59:36 -0500

    fixed missing material in HW2.m

--- HW2.m
+++ HW2.m
@@ -1,9 +1,9 @@
-L=[5 6; 2 3];   %our input matrix of displacement gradients
+L=[5 -3; 6 2];   %our input matrix of displacement gradients
 
 
 %compute symmetric and antisymmetric parts of L
-Lsym  = (L + L')/2;
-Lanti = (L - L')/2;
+Lsym  = (L - L')/2;
+Lanti = (L + L')/2;
 
 %show them in window so that you can see the difference (would get same
 %effect by removing the semicolon in the above lines)
@@ -12,3 +12,11 @@ Lsym
 Lanti
 
 
+[U,E]=eigs(Lsym) 
+%U is the matrix of eigenvectors, each column is a vector. 
+%E is a diagonal matrix where the diagonals are the eigenvalues
+%note you could call these whatever you want, but these letters are often
+%used.  
+
+
+%type help eigs to get more info.
